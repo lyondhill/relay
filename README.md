434fa03735f02d37d4036b1a9db952772482053b
Lyon Hill <lyondhill@gmail.com>
2017-08-03 20:18:46 -0600

    make updates to the clearity of the code

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Relay allows non communicative servers to communicate with each other through an
 Relay works by establishing a TCP listener that waits for connections to come in. Once a connection
 is established relay will attempt to establish a new TCP listener to facilitate this relay client's connections.
 Relay will then report to the relay client the port it has available for communication. At this point Both the 
-relay server and the relay client switch their standard tcp connection to a (TCP multiplexer)[https://en.wikipedia.org/wiki/Multiplexing]. From this point on any connections that are established on the new TCP listener will create a new multiplex'ed connection to the relay client and forward all traffic through the server to the relay client.
+relay server and the relay client switch their standard tcp connection to a [TCP multiplexer](https://en.wikipedia.org/wiki/Multiplexing). From this point on any connections that are established on the new TCP listener will create a new multiplex'ed connection to the relay client and forward all traffic through the server to the relay client.
 
 ## How to use it
 
@@ -29,4 +29,4 @@ The relay binary can be used in one of two ways:
 
 ### Integrate relay into your server
   
-  Relays communication is simple. First you establish a connection to the relay server. Once the connection is established relay will send a plain text string followed by a new line character. The string is the port it was able to establish for you to use to establish new connections to the relay server. For example if the server is able to establish a listener on port 4532 the first thing the server would send back would be `4532\n`. At this point the next thing that needs to be done is to escalate the tcp connection to a multiplexed server. The relay client is a server so in the multiplexed connection it establishes itself as a server. The specific multiplexing protocol relay uses is defined by (yamux)[https://github.com/hashicorp/yamux].
+  Relays communication is simple. First you establish a connection to the relay server. Once the connection is established relay will send a plain text string followed by a new line character. The string is the port it was able to establish for you to use to establish new connections to the relay server. For example if the server is able to establish a listener on port 4532 the first thing the server would send back would be `4532\n`. At this point the next thing that needs to be done is to escalate the tcp connection to a multiplexed server. The relay client is a server so in the multiplexed connection it establishes itself as a server. The specific multiplexing protocol relay uses is defined by [yamux](https://github.com/hashicorp/yamux).
--- relay.go
+++ relay.go
@@ -13,17 +13,71 @@ import (
 	"github.com/hashicorp/yamux"
 )
 
+
+// handle all incoming relay connections
 func handleRelayRequest(relayConn net.Conn) {
-	i, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		log.Fatalf("unable to parse port number %s", os.Args[1])
-	}
 
 	// reguardless of the outcome of this function
 	// we always want to close the proxy request
 	defer relayConn.Close()
 
 	// establish a new listener
+	listener, err := establishListener(relayConn)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// switch to a multiplexed connection
+	// because the proxy request client is actually the server
+	// I will take the roll of a client
+	session, err := yamux.Client(relayConn, yamux.DefaultConfig())
+	if err != nil {
+		log.Printf("unable to multiplex the relay client connection %s", err)
+		return
+	}
+
+	// fire up the listener go routine and start reading from channels
+	connChan := connChanListener(listener)
+
+	// create a time so we can check to make sure the relay client is still connected
+	timeChan := time.Tick(1 * time.Second)
+
+	// listen for new connections on the new listener
+	for {
+
+		select {
+		case clientConn, ok := <-connChan:
+			if !ok {
+				return
+			}
+			go handleClientConn(clientConn, session)
+
+		case <-timeChan:
+			if session.IsClosed() {
+				listener.Close()
+				break
+			}
+		}
+	}
+
+
+
+	// if we get here we went through all the avaliable ports on the
+	// machine and were unable to find an available one
+	log.Fatalln("Unable to establish a relay listener")
+}
+
+// attempt to establish a listener
+// on established connection inform relayconn how to connect to it
+func establishListener(relayConn net.Conn) (net.Listener, error) {
+
+	// make the port unber more loopable
+	i, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("unable to parse port number %s", os.Args[1])
+	}
+
+	// start at the port number we were given and attempt incremental increases
 	for ; i <= 65535; i++ {
 		listener, err := net.Listen("tcp", fmt.Sprintf(":%d", i))
 		if err != nil {
@@ -35,51 +89,16 @@ func handleRelayRequest(relayConn net.Conn) {
 		// inform the connection of the newly established port number
 		_, err = fmt.Fprintf(relayConn, "%d\n", i)
 		if err != nil {
-			log.Printf("Unable to communicate with the relay client %s", err)
-			return
+			return nil, fmt.Errorf("Unable to communicate with the relay client %s", err)
 		}
 
-		// switch to a multiplexed connection
-		// because the proxy request client is actually the server
-		// I will take the roll of a client
-		session, err := yamux.Client(relayConn, yamux.DefaultConfig())
-		if err != nil {
-			log.Printf("unable to multiplex the relay client connection %s", err)
-			return
-		}
+		return listener, nil
+	}	
 
-		// fire up the listener go routine and start reading from channels
-		connChan := connChanListener(listener)
-
-		// create a time so we can check to make sure the relay client is still connected
-		timeChan := time.Tick(1 * time.Second)
-
-		// listen for new connections on the new listener
-		for {
-
-			select {
-			case clientConn, ok := <-connChan:
-				if !ok {
-					return
-				}
-				go handleClientConn(clientConn, session)
-
-			case <-timeChan:
-				if session.IsClosed() {
-					listener.Close()
-					break
-				}
-			}
-		}
-
-	}
-
-	// if we get here we went through all the avaliable ports on the
-	// machine and were unable to find an available one
-	log.Fatalln("Unable to establish a relay listener")
+	return nil, fmt.Errorf("Unable to establish a relay listener")
 }
 
-// make the listener process a go routine so it can be shut down if the proxy shuts down
+// Make the accept process a go routine so it can be shut down if the proxy shuts down
 func connChanListener(listener net.Listener) chan net.Conn {
 	netChannel := make(chan net.Conn)
 
